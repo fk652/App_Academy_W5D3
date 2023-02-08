95b2a8b25cf55918dacf07cafa0b130ffcad1c61
“Kelsang” <kelsangrtsering@gmail.com>
2023-02-08 11:57:25 -0500

    Work on import_db.sql

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON; 
+
 DROP TABLE IF EXISTS users
 
 CREATE TABLE users (
@@ -9,10 +11,41 @@ CREATE TABLE users (
 DROP TABLE IF EXISTS questions
 
 CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
   title VARCHAR(200) NOT NULL,
   body TEXT NOT NULL,
-  user_id INTEGER NOT NULL,
-
-  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS question_follows
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+DROP TABLE IF EXISTS replies
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  parent_id INTEGER,
+  body TEXT NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id)
+);
+
+DROP TABLE IF EXISTS question_likes
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
