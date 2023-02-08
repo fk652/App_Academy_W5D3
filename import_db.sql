6f90bbf1045f0ba95ae59426bb8fd8f70a222af5
“Kelsang” <kelsangrtsering@gmail.com>
2023-02-08 16:17:58 -0500

    create questions.db file

--- import_db.sql
+++ import_db.sql
@@ -1,10 +1,10 @@
 PRAGMA foreign_keys = ON; 
 
-DROP TABLE IF EXISTS question_follows
-DROP TABLE IF EXISTS question_likes
-DROP TABLE IF EXISTS replies
-DROP TABLE IF EXISTS users
-DROP TABLE IF EXISTS questions
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
 
 
 CREATE TABLE users (
@@ -18,7 +18,7 @@ CREATE TABLE users (
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title VARCHAR(200) NOT NULL,
-  body TEXT NOT NULL,
+  body TEXT NOT NULL
 );
 
 
@@ -36,10 +36,12 @@ CREATE TABLE question_follows (
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
   question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
   parent_id INTEGER,
   body TEXT NOT NULL,
 
   FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (parent_id) REFERENCES replies(id)
 );
 
@@ -69,8 +71,8 @@ VALUES
 
 INSERT INTO
   questions (title, body)
-VALUES,
-  ('Sky', 'What color is the sky?')
+VALUES
+  ('Sky', 'What color is the sky?'),
   ('Mirror', "What's the color of a mirror?"),
   ('Date', "What's today's date?"),
   ('You?', 'Who are you?'),
@@ -102,9 +104,60 @@ VALUES
 
 
 INSERT INTO
-  replies
+  replies (question_id, user_id, parent_id, body)
 VALUES
+  (
+    (SELECT id FROM questions WHERE title = 'Sky'),
+    (SELECT id FROM users WHERE fname = 'Chris'),
+    NULL,
+    "red"
+  ),
+  (
+    (SELECT id FROM questions WHERE title = 'Sky'),
+    (SELECT id FROM users WHERE fname = 'Will'),
+    (SELECT id FROM replies WHERE body = 'red'),
+    "No, it's pink"
+  ),
+  (
+    (SELECT id FROM questions WHERE title = 'Sky'),
+    (SELECT id FROM users WHERE fname = 'Vin'),
+    (SELECT id FROM replies WHERE body = "No, it's pink"),
+    "No, it's flat"
+  ),
+  (
+    (SELECT id FROM questions WHERE title = 'Date'),
+    (SELECT id FROM users WHERE fname = 'Dwayne'),
+    NULL,
+    "2-8-23"
+  ),
+  (
+    (SELECT id FROM questions WHERE title = 'Date'),
+    (SELECT id FROM users WHERE fname = 'Tom' AND lname = 'Holland'),
+    (SELECT id FROM replies WHERE body = "2-8-23"),
+    "No I don't think you're right! It's friday!"
+  );
 
 INSERT INTO
-  question_likes
+  question_likes (user_id, question_id)
 VALUES
+(
+    (SELECT id FROM users WHERE fname = 'Dwayne'),
+    (SELECT id FROM questions WHERE title = 'Me?')
+  ),
+  (
+    (SELECT id FROM users WHERE fname = 'Vin'),
+    (SELECT id FROM questions WHERE title = 'You?')
+  ),
+  (
+    (SELECT id FROM users WHERE fname = 'Kevin'),
+    (SELECT id FROM questions WHERE title = 'Mirror')
+  ),
+  (
+    (SELECT id FROM users WHERE fname = 'Kelsang'),
+    (SELECT id FROM questions WHERE title = 'Sky')
+  ),
+  (
+    (SELECT id FROM users WHERE fname = 'Fahim'),
+    (SELECT id FROM questions WHERE title = 'Date')
+  );
+
